7350655bcd8aac05755960f63309757481ceb304
Hayder Aly <007harryjutt@gmail.com>
2023-04-28 20:51:48 +0500

    portfolio backend

---
+++ src/api/portfolio-overview/services/portfolio-overview.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * portfolio-overview service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::portfolio-overview.portfolio-overview');
